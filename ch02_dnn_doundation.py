c07f41d18f008584f9c3a3c4966747828df7c5a5
quantumliu <liuyiliang100@sina.com>
2022-07-23 19:11:52 +0800

    Add two output examples

--- ch02_dnn_doundation.py
+++ ch02_dnn_doundation.py
@@ -1,16 +1,33 @@
 import tensorlayerx as tlx
 # Single neuro
 
-## matrix multiplication
-x=tlx.constant([[1,2,3]])
-w=tlx.constant([[-0.5],[0.2],[0.1]])
+# matrix multiplication
+x = tlx.constant([[1, 2, 3]])
+w = tlx.constant([[-0.5], [0.2], [0.1]])
 
-print("X:\n",x,"\nShape:",x.shape)
-print("W:\n",w,"\nShape",w.shape)
+print("X:\n", x, "\nShape:", x.shape)
+print("W:\n", w, "\nShape", w.shape)
 
-## Bias
-b=tlx.constant(0.5)
+# Bias
+b = tlx.constant(0.5)
 
-z=tlx.matmul(x,w)+b
+z = tlx.matmul(x, w)+b
 
-print("Z:\n",z,"\nShape",z.shape)
+print("Z:\n", z, "\nShape", z.shape)
+
+# Two outputs
+
+x = tlx.constant([[1, 2, 3]])
+w = tlx.constant([[-0.5, -0.3],
+                  [0.2, 0.4],
+                  [0.1, 0.15]])
+
+print("X:\n", x, "\nShape:", x.shape)
+print("W:\n", w, "\nShape", w.shape)
+
+# Bias
+b = tlx.constant(0.5)
+
+z = tlx.matmul(x, w)+b
+
+print("Z:\n", z, "\nShape", z.shape)
